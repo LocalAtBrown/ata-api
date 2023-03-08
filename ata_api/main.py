ad0fff358a2828f36cb97d73753e53e1c8607e70
Raaid <raaid@protonmail.com>
2023-03-08 13:20:05 -0500

    add read_prescription first pass

--- ata_api/main.py
+++ ata_api/main.py
@@ -1,12 +1,23 @@
+from ata_db_models.helpers import get_conn_string
+from ata_db_models.models import Prescription
 from fastapi import FastAPI
 from mangum import Mangum
+from sqlmodel import Session, create_engine, select
 
 app = FastAPI()
+engine = create_engine(url=get_conn_string())
 
 
 @app.get("/")
 def get_root():
-    return {"message": "FastAPI running in a Lambda function"}
+    return {"message": "This is the root endpoint for the AtA API."}
+
+
+@app.get("/prescription/{user_id}", response_model=list[Prescription])
+def read_prescription(user_id: str):
+    with Session(engine) as session:
+        prescription = session.exec(select(Prescription).where(Prescription.user_id == user_id)).first()
+        return prescription
 
 
 handler = Mangum(app)
