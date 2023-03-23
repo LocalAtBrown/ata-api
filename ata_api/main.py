6a4948549cc9ad23b54cf53410e0b0114e47e447
Raaid <raaid@protonmail.com>
2023-03-23 16:18:32 -0400

    have an RCT option via fair coin toss, and have the option to grab prescriptions from the db

--- ata_api/main.py
+++ ata_api/main.py
@@ -1,3 +1,6 @@
+from os import getenv
+from random import randint
+
 from ata_db_models.helpers import get_conn_string
 from ata_db_models.models import Prescription
 from fastapi import FastAPI
@@ -6,6 +9,7 @@ from sqlmodel import Session, create_engine, select
 
 app = FastAPI()
 engine = create_engine(url=get_conn_string())
+STRATEGY = getenv("STRATEGY", "RCT")
 
 
 @app.get("/")
@@ -14,12 +18,23 @@ def get_root():
 
 
 @app.get("/prescription/{site_name}/{user_id}", response_model=list[Prescription])
-def read_prescription(site_name: str, user_id: str):
+def read_prescription(site_name: str, user_id: str) -> int:
+    if STRATEGY == "RCT":
+        return fair_coin_toss()
+    else:
+        return get_prescription_from_db(site_name=site_name, user_id=user_id)
+
+
+def fair_coin_toss() -> int:
+    return randint(0, 1)
+
+
+def get_prescription_from_db(site_name: str, user_id: str) -> int:
     with Session(engine) as session:
         prescription = session.exec(
             select(Prescription).where(Prescription.user_id == user_id, Prescription.site_name == site_name)
         ).first()
-        return prescription
+        return int(prescription.prescribe)
 
 
 handler = Mangum(app)
