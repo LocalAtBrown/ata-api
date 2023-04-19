67b9d87cfe086ddc165dc9cbe1f9f5c0a57db425
Raaid <raaid@protonmail.com>
2023-04-19 13:16:35 -0400

    Add simple endpoint (#1)
    * add ata-db-models and fastapi
    
    * add mangum dependency
    
    * add simple scaffold
    
    * add read_prescription first pass
    
    * update endpoint to account for site name
    
    * have an RCT option via fair coin toss, and have the option to grab prescriptions from the db
    
    * change up dockerfile
    
    * add uvicorn in dev dependency, update readme, fix return type of prescription endpoint
    
    * pickup up where I left off a bit ago, this looks fine for the starting point
    
    * appease mypy
    
    * remove test running CI as we dont have tests for this yet anyway
    
    * change env var name from STRATEGY to POLICY

---
+++ ata_api/main.py
@@ -0,0 +1,47 @@
+from os import getenv
+from random import randint
+
+from ata_db_models.helpers import get_conn_string
+from ata_db_models.models import Prescription
+from fastapi import FastAPI
+from mangum import Mangum
+from sqlmodel import Session, create_engine, select
+
+app = FastAPI()
+engine = create_engine(url=get_conn_string())
+POLICY = getenv("POLICY", "RCT")
+
+
+@app.get("/")
+def get_root() -> object:
+    return {"message": "This is the root endpoint for the AtA API."}
+
+
+@app.get("/prescription/{site_name}/{user_id}", response_model=int)
+def read_prescription(site_name: str, user_id: str) -> int:
+    if POLICY == "RCT":
+        return fair_coin_toss()
+    else:
+        return get_prescription_from_db(site_name=site_name, user_id=user_id)
+
+
+def fair_coin_toss() -> int:
+    return randint(0, 1)
+
+
+def ignore_existing_subscribers() -> bool:
+    # todo
+    ...
+
+
+def get_prescription_from_db(site_name: str, user_id: str) -> int:
+    with Session(engine) as session:
+        prescription = session.exec(
+            select(Prescription).where(Prescription.user_id == user_id, Prescription.site_name == site_name)
+        ).first()
+        if prescription:
+            return int(prescription.prescribe)
+        return 0
+
+
+handler = Mangum(app)
