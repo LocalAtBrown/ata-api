b357ac50236a3c83b1927cf3f63331ed56af3305
Raaid <raaid@protonmail.com>
2023-04-26 13:43:30 -0400

    add a more detailed response model and edge-case handling

--- ata_api/main.py
+++ ata_api/main.py
@@ -4,10 +4,16 @@ from ata_db_models.helpers import get_conn_string
 from ata_db_models.models import Group, UserGroup
 from fastapi import FastAPI
 from mangum import Mangum
-from sqlmodel import Session, create_engine, select
+from pydantic import BaseModel
+from sqlmodel import Session, create_engine, select, text
 
 app = FastAPI()
 engine = create_engine(url=get_conn_string())
+
+
+class PrescriptionResponse(BaseModel):
+    group: Group
+    value: int
 
 
 @app.get("/")
@@ -15,38 +21,61 @@ def get_root() -> object:
     return {"message": "This is the root endpoint for the AtA API."}
 
 
-@app.get("/prescription/{site_name}/{user_id}", response_model=int)
-def read_prescription(site_name: str, user_id: str) -> int:
-    # if no row for the user/site, create it
-    # based on the group, return the prescription
-    # A: original (so... 1?)
-    # B: run scroll depth calc
-    # C: no show (0)
+@app.get("/prescription/{site_name}/{user_id}", response_model=PrescriptionResponse)
+def read_prescription(site_name: str, user_id: str) -> PrescriptionResponse:
     with Session(engine) as session:
         # query db for user/site group
         usergroup = session.exec(
             select(UserGroup).where(UserGroup.user_id == user_id, UserGroup.site_name == site_name)
         ).first()
-        # if no row for the user/site, create it (so if usergroup is none/empty
+        # if no row for the user/site, create it
         if not usergroup:
             usergroup = UserGroup(user_id=UUID(user_id), site_name=site_name)
             session.add(usergroup)
             session.commit()
-        # TODO insert user_id + site_name, with RETURNING statement for group
+
         if usergroup.group == Group.A:
-            # show existing CTA
-            return 1
+            # show CTA
+            return PrescriptionResponse(group=Group.A, value=1)
         elif usergroup.group == Group.B:
             # model
-            # TODO
-            # if not enough data to do this, return default
-            return -1
+            statement = text(
+                f"""
+            WITH prep AS (SELECT LEAST(
+                                 LEAST(
+                                         GREATEST(
+                                                 MAX(coalesce(ev.pp_yoffset_max, 0)),
+                                                 0),
+                                         MAX(ev.doc_height)
+                                     ) + MAX(ev.br_viewheight),
+                                 MAX(ev.doc_height)
+                         ) / MAX(ev.doc_height)::FLOAT
+                         as relative_vmax
+                          FROM event AS ev
+                          WHERE ev.doc_height > 0
+                            AND ev.domain_userid = '{user_id}'
+                            AND ev.site_name = '{site_name}'
+                          GROUP BY domain_sessionidx, page_urlpath)
+            SELECT avg(relative_vmax) AS avg_relative_vmax
+            FROM prep;
+            """
+            )
+            scroll_depth = session.execute(statement).first()
+
+            try:
+                if scroll_depth[0] > 0.3:
+                    PrescriptionResponse(group=Group.B, value=1)
+                else:
+                    return PrescriptionResponse(group=Group.B, value=0)
+            except TypeError:
+                # not enough data to calculate, return a default
+                return PrescriptionResponse(group=Group.B, value=0)
         elif usergroup.group == Group.C:
             # no show
-            return 0
+            return PrescriptionResponse(group=Group.C, value=0)
         else:
             # shouldn't get here, safe default of 0
-            return 0
+            return PrescriptionResponse(group=Group.C, value=0)
 
 
 handler = Mangum(app)
