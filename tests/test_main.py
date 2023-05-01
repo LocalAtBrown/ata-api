6e432db1be55510ba877916380ab5e0587e9aa62
Alex Goncalves <alexandre.reportagem@gmail.com>
2023-05-01 15:06:12 -0400

    add return None to test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ from ata_api.main import app
 client = TestClient(app)
 
 
-def test_dummy_site():
+def test_dummy_site() -> None:
     response = client.get("/prescription/dummysite/")
     assert response.status_code == 404
     assert response.json() == {
@@ -13,7 +13,7 @@ def test_dummy_site():
     }
 
 
-def test_no_user():
+def test_no_user() -> None:
     response = client.get("/prescription/the-19th/")
     assert response.status_code == 404
     assert response.json() == {
@@ -21,7 +21,7 @@ def test_no_user():
     }
 
 
-def test_invalid_uuid():
+def test_invalid_uuid() -> None:
     response = client.get("/prescription/the-19th/dummyuser")
     assert response.status_code == 404
     assert response.json() == {
@@ -29,15 +29,7 @@ def test_invalid_uuid():
     }
 
 
-def test_invalid_uuid():
-    response = client.get("/prescription/the-19th/dummyuser")
-    assert response.status_code == 404
-    assert response.json() == {
-        "detail": "Invalid user ID: dummyuser",
-    }
-
-
-def test_invalid_user_and_site():
+def test_invalid_user_and_site() -> None:
     response = client.get("/prescription/dummysite/bc9d39e6-bb74-4bbd-8048-b7ac54054982")
     assert response.status_code == 404
     assert response.json() == {
