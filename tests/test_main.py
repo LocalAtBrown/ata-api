2aaec914f0f1b7e99f0c32fbc8aad7e7c74f0a1a
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-05-05 16:01:31 -0400

    [remove] tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,22 +5,6 @@ from ata_api.main import app
 client = TestClient(app)
 
 
-def test_dummy_site() -> None:
-    response = client.get("/prescription/dummysite/")
-    assert response.status_code == 404
-    assert response.json() == {
-        "detail": "Invalid site: dummysite. You also need to specify a user ID",
-    }
-
-
-def test_no_user() -> None:
-    response = client.get("/prescription/the-19th/")
-    assert response.status_code == 404
-    assert response.json() == {
-        "detail": "Please, specify a user ID",
-    }
-
-
 def test_invalid_uuid() -> None:
     response = client.get("/prescription/the-19th/dummyuser")
     assert response.status_code == 404
@@ -29,17 +13,9 @@ def test_invalid_uuid() -> None:
     }
 
 
-def test_invalid_user_and_site() -> None:
+def test_invalid_site() -> None:
     response = client.get("/prescription/dummysite/bc9d39e6-bb74-4bbd-8048-b7ac54054982")
     assert response.status_code == 404
     assert response.json() == {
         "detail": "Invalid site: dummysite",
-    }
-
-
-def test_only_prescription() -> None:
-    response = client.get("/prescription/")
-    assert response.status_code == 404
-    assert response.json() == {
-        "detail": "Please specify a site and a user ID",
     }
