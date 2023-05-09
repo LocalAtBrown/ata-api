4906237fd90808310caa09b1af5c52663be8d064
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-05-09 12:15:07 -0400

    [change] tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,22 @@
 from fastapi.testclient import TestClient
+from fastapi import status
 
 from ata_api.main import app
 
 client = TestClient(app)
 
 
-def test_invalid_uuid() -> None:
+def test_invalid_user_id() -> None:
     response = client.get("/prescription/the-19th/dummyuser")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {
         "detail": "Invalid user ID: dummyuser",
     }
 
 
-def test_invalid_site() -> None:
+def test_invalid_site_name() -> None:
     response = client.get("/prescription/dummysite/bc9d39e6-bb74-4bbd-8048-b7ac54054982")
-    assert response.status_code == 404
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
     assert response.json() == {
         "detail": "Invalid site: dummysite",
     }
