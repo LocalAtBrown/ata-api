85d6c77b562d82d4edf3ab6cb138c3ae4027d3bd
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-05-09 23:04:40 -0400

    [add] first test

--- ata_api/main.py
+++ ata_api/main.py
@@ -2,10 +2,11 @@ from uuid import UUID
 
 from ata_db_models.helpers import get_conn_string
 from ata_db_models.models import Group, UserGroup
-from fastapi import FastAPI, HTTPException, status
+from fastapi import Depends, FastAPI, HTTPException, status
 from mangum import Mangum
 from pydantic import BaseModel
-from sqlmodel import Session, create_engine, select
+from sqlalchemy.orm import Session, sessionmaker
+from sqlmodel import create_engine, select
 
 from ata_api.helpers.enums import SiteName
 from ata_api.helpers.logging import logging
@@ -14,6 +15,15 @@ logger = logging.getLogger(__name__)
 
 app = FastAPI()
 engine = create_engine(url=get_conn_string())
+session_factory = sessionmaker(autoflush=False, autocommit=False, bind=engine)
+
+
+def create_db_session():
+    session = session_factory()
+    try:
+        yield session
+    finally:
+        session.close()
 
 
 class PrescriptionResponse(BaseModel):
@@ -28,7 +38,9 @@ def get_root() -> object:
 
 
 @app.get("/prescription/{site_name}/{user_id}", response_model=PrescriptionResponse)
-def get_or_create_prescription(site_name: str, user_id: str) -> PrescriptionResponse:
+def get_or_create_prescription(
+    site_name: str, user_id: str, session: Session = Depends(create_db_session)
+) -> PrescriptionResponse:
     # Verify if user_id is a valid UUID string of 32 hexadecimal digits
     try:
         user_id_uuid = UUID(user_id)
@@ -39,29 +51,28 @@ def get_or_create_prescription(site_name: str, user_id: str) -> PrescriptionResp
     if site_name not in {*SiteName}:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=f"Invalid site: {site_name}")
 
-    with Session(engine) as session:
-        # query db for user/site group
+    # query db for user/site group
+    try:
+        usergroup = session.execute(
+            select(UserGroup).where(UserGroup.user_id == user_id_uuid, UserGroup.site_name == site_name)
+        ).first()
+    except Exception as e:
+        logger.exception(f"Site: {site_name}, user ID: {user_id}: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="There was a problem with the database"
+        )
+
+    # if no row for the user/site, create it
+    if not usergroup:
         try:
-            usergroup = session.exec(
-                select(UserGroup).where(UserGroup.user_id == user_id_uuid, UserGroup.site_name == site_name)
-            ).first()
+            usergroup = UserGroup(user_id=user_id_uuid, site_name=site_name)
+            session.add(usergroup)
+            session.commit()
         except Exception as e:
             logger.exception(f"Site: {site_name}, user ID: {user_id}: {e}")
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="There was a problem with the database"
             )
-
-        # if no row for the user/site, create it
-        if not usergroup:
-            try:
-                usergroup = UserGroup(user_id=user_id_uuid, site_name=site_name)
-                session.add(usergroup)
-                session.commit()
-            except Exception as e:
-                logger.exception(f"Site: {site_name}, user ID: {user_id}: {e}")
-                raise HTTPException(
-                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="There was a problem with the database"
-                )
 
     return PrescriptionResponse(site_name=site_name, user_id=user_id, group=usergroup.group)
 
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,22 +1,75 @@
-from fastapi.testclient import TestClient
-from fastapi import status
+from typing import Generator, Tuple
+from uuid import UUID
 
-from ata_api.main import app
+import pytest
+from ata_db_models.models import Group, SQLModel
+from fastapi import status
+from fastapi.testclient import TestClient
+from sqlalchemy import func
+from sqlmodel import select
+
+from ata_api.helpers.enums import SiteName
+from ata_api.main import UserGroup, app, engine, session_factory
 
 client = TestClient(app)
 
 
-def test_invalid_user_id() -> None:
-    response = client.get("/prescription/the-19th/dummyuser")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {
-        "detail": "Invalid user ID: dummyuser",
-    }
+@pytest.fixture(scope="function")
+def create_and_drop_tables() -> Generator[None, None, None]:
+    """
+    Fixture responsible for creating tables before and dropping tables after
+    every test, ensuring a clean slate. (See: https://docs.pytest.org/en/6.2.x/fixture.html#:~:text=%E2%80%9CYield%E2%80%9D%20fixtures%20yield%20instead%20of,is%20swapped%20out%20for%20yield%20.)
+
+    If using a dedicated test DB instead of localhost, try the https://dev.to/jbrocher/fastapi-testing-a-database-5ao5 approach,
+    which right now is overkill.
+    """
+    SQLModel.metadata.create_all(engine)
+    yield
+    SQLModel.metadata.drop_all(engine)
 
 
-def test_invalid_site_name() -> None:
-    response = client.get("/prescription/dummysite/bc9d39e6-bb74-4bbd-8048-b7ac54054982")
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {
-        "detail": "Invalid site: dummysite",
-    }
+class TestRoot:
+    @pytest.mark.unit
+    def test_root(self) -> None:
+        response = client.get("/")
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == {"message": "This is the root endpoint for the AtA API."}
+
+
+class TestPrescription:
+    @pytest.fixture(scope="class")
+    def user(self) -> Tuple[str, str]:
+        return (SiteName.AFRO_LA, "3800ac11781a4cf2a6759bbaa9c0729b")
+
+    @pytest.mark.unit
+    def test_invalid_user_id(self, user) -> None:
+        response = client.get(f"/prescription/{user[0]}/dummyuser")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {
+            "detail": "Invalid user ID: dummyuser",
+        }
+
+    @pytest.mark.unit
+    def test_invalid_site_name(self, user) -> None:
+        response = client.get(f"/prescription/dummysite/{user[1]}")
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {
+            "detail": "Invalid site: dummysite",
+        }
+
+    @pytest.mark.integration
+    def test_user_not_exist(self, create_and_drop_tables, user) -> None:
+        # with create_and_drop_tables(engine):
+        response = client.get(f"/prescription/{'/'.join(user)}")
+        assert response.status_code == status.HTTP_200_OK
+
+        # Check returned data is of the right user
+        data = response.json()
+        assert data["site_name"] == user[0]
+        assert UUID(data["user_id"]) == UUID(user[1])
+        assert data["group"] in {*Group}  # A, B or C
+
+        # Check data is written to DB
+        with session_factory() as session:
+            count = session.execute(select(func.count()).select_from(select(UserGroup).subquery())).scalar_one()
+            assert count == 1
