dd6a2bdd90cb451cbee039abae997683e026d456
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-05-15 15:43:15 -0400

    [change] script input to total number of paths

--- tests/performance/generate_fake_paths.py
+++ tests/performance/generate_fake_paths.py
@@ -4,9 +4,12 @@ from typing import Tuple
 from uuid import UUID, uuid4
 
 import click
+import numpy as np
 import pandas as pd
 
 from ata_api.helpers.enums import SiteName
+
+rng = np.random.default_rng()
 
 
 @dataclass
@@ -19,25 +22,32 @@ class User:
 
 
 @click.command()
-@click.option("--afro-la", default=0, type=int)
-@click.option("--dallas-free-press", default=0, type=int)
-@click.option("--open-vallejo", default=0, type=int)
-@click.option("--the-19th", default=0, type=int)
-def main(afro_la: int, dallas_free_press: int, open_vallejo: int, the_19th: int) -> None:
+@click.option("-n", "--num-samples", required=True, type=int)
+@click.option("--freq-afla", default=0.1, type=float)
+@click.option("--freq-dfp", default=0.2, type=float)
+@click.option("--freq-ov", default=0.2, type=float)
+@click.option("--freq-19", default=0.5, type=float)
+def main(num_samples: int, freq_afla: float, freq_dfp: float, freq_ov: float, freq_19: float) -> None:
+    num_afro_la, num_dallas_free_press, num_open_vallejo, num_the_19th = rng.multinomial(
+        n=num_samples, pvals=[freq_afla, freq_dfp, freq_ov, freq_19]
+    )
+
     paths = pd.Series(
         [
-            *[User(site_name=SiteName.AFRO_LA).convert_to_path() for _ in range(afro_la)],
-            *[User(site_name=SiteName.DALLAS_FREE_PRESS).convert_to_path() for _ in range(dallas_free_press)],
-            *[User(site_name=SiteName.OPEN_VALLEJO).convert_to_path() for _ in range(open_vallejo)],
-            *[User(site_name=SiteName.THE_19TH).convert_to_path() for _ in range(the_19th)],
+            *[User(site_name=SiteName.AFRO_LA).convert_to_path() for _ in range(num_afro_la)],
+            *[User(site_name=SiteName.DALLAS_FREE_PRESS).convert_to_path() for _ in range(num_dallas_free_press)],
+            *[User(site_name=SiteName.OPEN_VALLEJO).convert_to_path() for _ in range(num_open_vallejo)],
+            *[User(site_name=SiteName.THE_19TH).convert_to_path() for _ in range(num_the_19th)],
         ],
         name="path",
     )
 
     assert paths.nunique() == len(paths)
 
-    paths.to_csv(
-        f"{Path(__file__).parent}/paths_{afro_la}_{dallas_free_press}_{open_vallejo}_{the_19th}.csv", index=False, header=False
+    paths.sample(frac=1).to_csv(
+        f"{Path(__file__).parent}/paths_{num_samples}.csv",
+        index=False,
+        header=False,
     )
 
 
