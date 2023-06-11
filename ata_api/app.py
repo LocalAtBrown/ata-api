1b984d061e276469e9b37aec3c00aa6d4c104a6a
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-06-11 18:21:38 -0400

    Add CORS middleware (#11)
    * [add] boto3 dependency
    
    * [add] cors middleware
    
    * [add] CORS_ALLOWED_ORIGINS defaulting to empty list

--- ata_api/app.py
+++ ata_api/app.py
@@ -1,13 +1,31 @@
+import json
+import os
+
 from fastapi import FastAPI, Request, status
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
 from starlette.exceptions import ExceptionMiddleware
 
 from ata_api.monitoring.logging import logger
 from ata_api.routing import LoggerRouteHandler
 
+
+def get_cors_allowed_origins() -> list[str]:
+    origins = os.environ.get("CORS_ALLOWED_ORIGINS")
+
+    if origins is None:
+        logger.warning("CORS_ALLOWED_ORIGINS not set, defaulting to empty list")
+        return []
+
+    logger.info(f"CORS_ALLOWED_ORIGINS set to {origins}")
+    return json.loads(origins)  # type: ignore
+
+
 app = FastAPI()
 # Add FastAPI context to logs
 app.router.route_class = LoggerRouteHandler
+# Add CORS whitelist
+app.add_middleware(CORSMiddleware, allow_origins=get_cors_allowed_origins(), allow_methods=["GET"])
 # Add exception middleware to log unhandled exceptions
 app.add_middleware(ExceptionMiddleware, handlers=app.exception_handlers)
 
