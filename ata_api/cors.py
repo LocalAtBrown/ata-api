b616735ad4a8e019b82b12f85b7e857a900addba
Duy Nguyen <57234880+duynguyen158@users.noreply.github.com>
2023-06-13 18:17:05 -0400

    [change] cors decorator to just a function

--- ata_api/cors.py
+++ ata_api/cors.py
@@ -1,46 +1,17 @@
-import functools
-from typing import Callable, Union
+from typing import Optional
 
 from fastapi import Response
-from fastapi.encoders import jsonable_encoder
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
-from typing_extensions import ParamSpec
-
-from ata_api.settings import settings
-
-P = ParamSpec("P")
+from pydantic import HttpUrl
 
 
-def allow_cors(func: Callable[P, Union[BaseModel, Response]]) -> Callable[P, Union[BaseModel, Response]]:
+def evaluate_cors(response: Response, origin: Optional[HttpUrl], cors_allowed_origins: set[HttpUrl]) -> None:
     """
-    Wraps a FastAPI path function to allow CORS requests.
-    Works with or without the FastAPI CORS middleware.
+    Evaluates the origin of a request and updates the response headers accordingly.
     """
-
-    @functools.wraps(func)
-    def wrapper(*args: P.args, **kwargs: P.kwargs) -> Union[BaseModel, Response]:
-        output = func(*args, **kwargs)
-
-        origin = kwargs.get("origin")
-
-        # Still check if origin is allowed in case preflight request is not sent
-        if origin is None or origin not in settings.cors_allowed_origins:
-            return output
-
-        # If output is a BaseModel, return a JSONResponse with CORS headers
-        if isinstance(output, BaseModel):
-            return JSONResponse(
-                headers={
-                    "Access-Control-Allow-Origin": str(origin),
-                    "Vary": "Origin",
-                },
-                content=jsonable_encoder(output),
-            )
-        # If output is a Response, update its headers with CORS headers
-        elif isinstance(output, Response):
-            output.headers["Access-Control-Allow-Origin"] = str(origin)
-            output.headers.add_vary_header("Origin")
-            return output
-
-    return wrapper
+    if origin is not None and origin in cors_allowed_origins:
+        response.headers.update(
+            {
+                "Access-Control-Allow-Origin": str(origin),
+                "Vary": "Origin",
+            }
+        )
